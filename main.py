77b38920cb70f2715ef7ee37b68a4cfa352a9d70
Aleksandra Rabęda <rabeda.aleksandra@gmail.com>
2021-12-03 22:25:20 +0100

    Fryderyk's implementation

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import numpy as np
 import pygame
+import cv2
 
-background = pygame.image.load(background.png)
+matrix = []
+
+def slicer():
+    background = cv2.imread('background.png')
+    y = background.shape[0]
+    x = background.shape[1]
+    for i in range(0, int(y/25)):
+        for j in range(0, int(x/25)):
+            temp = Cell(25*i, 25*j)
+            matrix.append(temp)
+    return 0
 
 class Cell:
-    def __init__(self):
-        self.coordiantes_list = []
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        #self.color = color
+
+    def __str__(self):
+        return "{} {}".format(self.x, self.y)
+
+if __name__ == '__main__':
+    slicer()
+    [ print(str(x)) for x in matrix ]
